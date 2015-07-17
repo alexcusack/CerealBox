fe4a091c79ef8ab92b7bbd5c4628ae1176a2cbdf
Andrelton <eric.saxman@gmail.com>
2015-07-17 12:37:07 -0700

    complete preliminary ajax functionality for all three content types

--- learnwithme/app/assets/javascripts/new_content_ajax.js
+++ learnwithme/app/assets/javascripts/new_content_ajax.js
@@ -2,7 +2,7 @@ $(function() {
   $(document).on('ajax:success', 'form', function(event, newContent) {
     var contentType = $(this).attr('data-content');
     var destination = $('div.current-content').find('div.' + contentType);
-    destination.prepend(newContent);
-    // form.parent().prepend(newContent);
+    // this will become html
+    destination.prepend(newContent.title);
   });
 });
