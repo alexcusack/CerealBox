ccbd1a5c4e4768d025c6e0f0634ac5b139dde9cf
Andrelton <eric.saxman@gmail.com>
2015-07-17 20:19:12 -0700

    begin work on cancel button

--- learnwithme/app/assets/javascripts/editCourseContent.js
+++ learnwithme/app/assets/javascripts/editCourseContent.js
@@ -1,4 +1,3 @@
-// var editContentForm = React.createClass
 
 var editContentButton = {
   clickListen: function() {
@@ -11,24 +10,33 @@ var editContentButton = {
           url: action
         });
         request.done(function(editContentForm) {
-          var info = button.siblings('div.content-fields');
-          info.toggle();
+          // var info = button.siblings('div.content-fields');
+          // info.toggle();
           button.parent().append(editContentForm);
+          button.addClass('cancel').text('cancel');
         });
 
     });
   }
 };
 
+var cancelButton = {
+  clickListen: function() {
+    $('div.content').on('click', 'a.cancel', function(event) {
+      event.preventDefault();
+      alert('Yeahhh');
+    })
+  }
+}
+
 var editContentForm = {
   submitListen: function() {
     $(document).on('ajax:success', 'div.content form', function(event, updatedContent) {
       var form = $(this);
       form.closest('div.course-content').replaceWith(updatedContent);
-      // form.siblings('div.content-fields').remove();
-      // form.siblings('div.change-content-links').remove();
-      // form.parent().prepend(updatedContent);
-      // form.remove();
     });
   }
 };
+
+
+
