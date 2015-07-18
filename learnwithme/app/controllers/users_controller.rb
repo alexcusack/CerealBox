462219e9fb8eee3a7f9725b079ab4ff29343facc
Alex Cusack & Tanay Arora <pair+cusackalex.tarora2014@devbootcamp.com>
2015-07-18 13:20:45 -0700

    add sessions

--- learnwithme/app/controllers/users_controller.rb
+++ learnwithme/app/controllers/users_controller.rb
@@ -8,22 +8,23 @@ def create
     user_info = params['google']['cachedUserProfile']   if params['google']
     user_info = params['facebook']['cachedUserProfile'] if params['facebook']
     user = User.where(username: user_info['name']).first_or_initialize
-        user.first_name   = user_info['name'],
-        user.last_name    = user_info['family_name'],
-        user.username     = user_info['name'],
-        user.avatar_url   = user_info['picture'],
-        user.google_token = params['token'] if params['google']
-        )
+      user.first_name   = user_info['name'],
+      user.last_name    = user_info['family_name'],
+      user.username     = user_info['name'],
+      user.avatar_url   = user_info['picture'],
+      user.google_token = params['token'] if params['google']
+      )
     if params['facebook']
       user.first_name = user_info['first_name']
       user.avatar_url = user_info['picture']['url']
       user.facebook_token = params['token']
     end
     if user.save
-       render :json => { :status => 200}
+      session[:user_id] = user.id
+      render :json => { :status => 200}
     else
-       render :json => { :status => 400 },
-       status: 400
+      render :json => { :status => 400 },
+      status: 400
     end
   end
 end
