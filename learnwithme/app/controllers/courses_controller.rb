2739d435e5b740efd2923dadb0f53e2f9208984a
Tanay Arora <tarora@g.clemson.edu>
2015-07-18 12:39:48 -0700

    fix create user

--- learnwithme/app/controllers/courses_controller.rb
+++ learnwithme/app/controllers/courses_controller.rb
@@ -1,7 +1,11 @@
 class CoursesController < ApplicationController
   def index
+    if current_user
+      @user = User.find(current_user.id)
+      p "&" *100
+      p @user
+    end
     @courses = Course.all
-    @user = User.find(current_user.id)
   end
 
   def new
--- learnwithme/app/controllers/users_controller.rb
+++ learnwithme/app/controllers/users_controller.rb
@@ -11,21 +11,21 @@ class UsersController < ApplicationController
     p "*" * 100
     p user_info
     p "*" * 100
-    @user = User.where(username: user_info['name']).first_or_initialize
-    @user.first_name   = user_info['given_name']
-    @user.last_name    = user_info['family_name']
-    @user.username     = user_info['name']
-    @user.avatar_url   = user_info['picture'] if params['google']
-    @user.avatar_url = user_info['picture']['url'] if params['facebook']
-    @user.google_token = params['token'] if params['google']
-    @user.facebook_token = params['token'] if params['facebook']
+    user = User.where(username: user_info['name']).first_or_initialize
+    user.first_name   = user_info['given_name']
+    user.last_name    = user_info['family_name']
+    user.username     = user_info['name']
+    user.avatar_url   = user_info['picture'] if params['google']
+    user.avatar_url = user_info['picture']['url'] if params['facebook']
+    user.google_token = params['token'] if params['google']
+    user.facebook_token = params['token'] if params['facebook']
 
-    if @user.save
-      p @user
-       render :json => { :user => 200}
+    if user.save
+      session[:user_id] = user.id
+      render :json => { :user => 200}
     else
-       render :json => { :status => 400 },
-       status: 400
+      render :json => { :status => 400 },
+      status: 400
     end
   end
 end
