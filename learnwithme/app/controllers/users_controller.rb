4d156df845c306e03877bf7e0baae4f16bd4e96c
Vivek Poola <vpoola88@gmail.com>
2015-07-18 17:56:14 -0700

    fix from moving to cereal box

--- learnwithme/app/controllers/users_controller.rb
+++ learnwithme/app/controllers/users_controller.rb
@@ -8,12 +8,12 @@ def create
     user_info = params['google']['cachedUserProfile']   if params['google']
     user_info = params['facebook']['cachedUserProfile'] if params['facebook']
     user = User.where(username: user_info['name']).first_or_initialize
-      user.first_name   = user_info['name'],
-      user.last_name    = user_info['family_name'],
-      user.username     = user_info['name'],
-      user.avatar_url   = user_info['picture'],
-      user.google_token = params['token'] if params['google']
-      )
+    user.first_name   = user_info['given_name']
+    user.last_name    = user_info['family_name']
+    user.username     = user_info['name']
+    user.avatar_url   = user_info['picture']
+    user.google_token = params['token'] if params['google']
+
     if params['facebook']
       user.first_name = user_info['first_name']
       user.avatar_url = user_info['picture']['url']
