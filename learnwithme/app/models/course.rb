44079de6e5dc5ea965481c78bbad10f8857a85b7
Alex Cusack <cusackalex@gmail.com>
2015-07-18 18:24:54 -0700

    fix association and views

--- learnwithme/app/models/course.rb
+++ learnwithme/app/models/course.rb
@@ -3,7 +3,7 @@ class Course < ActiveRecord::Base
   belongs_to :owner,  foreign_key: "user_id", class_name: 'User'
 
   has_many :user_courses
-  has_many :members, through: :user_courses, class_name: 'User'
+  has_many :members, through: :user_courses, source: :user
 
   has_many :course_sheets
   has_many :sheets, through: :course_sheets , class_name: 'Sheet'
