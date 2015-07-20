db8eb4b58d40fb0b195019d6bb2312278cbbda3a
Alex Cusack & Tanay Arora <pair+cusackalex.tarora2014@devbootcamp.com>
2015-07-19 19:04:17 -0700

    WIP

--- learnwithme/app/helpers/application_helper.rb
+++ learnwithme/app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def self.say_hi
-    p "Helloooo $$$"
-  end
 end
--- learnwithme/app/models/google_plus.rb
+++ learnwithme/app/models/google_plus.rb
@@ -10,7 +10,6 @@ module GooglePlus
     response = HTTParty.get("#{BASE_URL}#{user.google_uid}/people/visible?alt=json&access_token=#{GooglePlus.access_token(user)}")
   end
 
-
 ## this works to fetch activites https://www.googleapis.com/plus/v1/people/114785600982231839455/activities/public?alt=json&access_token=ya29.tQGtBy64a3W2T-0XGu6SrnqhVrzAlcb8PAFVE5tUVy0ikeLoV8-JRUbpv5Dho5Goy8kydG-iiA
 ## us id and access_token (which is per user)
 ##this works to get friends  https://www.googleapis.com/plus/v1/people/114785600982231839455/people/visible?alt=json&access_token=ya29.tQGtBy64a3W2T-0XGu6SrnqhVrzAlcb8PAFVE5tUVy0ikeLoV8-JRUbpv5Dho5Goy8kydG-iiA
