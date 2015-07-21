6fff800ef27e5ab5e5074e0ebf8aba2ac1fcc4c9
tarora2014 <tarora@g.clemson.edu>
2015-07-21 10:55:39 -0700

    course joined email

--- app/mailers/signin_mailer.rb
+++ app/mailers/signin_mailer.rb
@@ -9,4 +9,13 @@ class SigninMailer < ActionMailer::Base
       )
   end
 
+  def joined_course(user,course)
+    mail(
+      to: user.email,
+      from: 'summerhottie9000@gmail.com',
+      subject: 'You have just joind '+course.title+'!'
+      body: "Thank you for pledging to "+course.tilte+". The course will start on: "+course.start_date+". Be sure to be there!
+      Thanks."
+      )
+
 end
